37d67b5f1069a92d2dad973c854faf04e5ccedea
RyleeThomas <48770643+RyleeThomas@users.noreply.github.com>
2021-11-28 03:04:47 -0500

    update

--- assets/js/holidaylistmaybe.js
+++ assets/js/holidaylistmaybe.js
@@ -1,11 +1,14 @@
-  //https request for holidays on 11/01/2021
-  fetch ("https://holidays.abstractapi.com/v1/?api_key=e2012bd7586b4502b4d64c61f9a7eed8&country=US&year=2021&month=11&day=1")
+  //https request for holidays 
+  fetch ("https://holidays.abstractapi.com/v1/?api_key=e2012bd7586b4502b4d64c61f9a7eed8&country=US&year=2021&month=11")
   .then (function (response){
   return response.json();
   })
   .then(function (data) {
       console.log(data);
   })
-    
+
+  // user pick country
+  // year and month picked from day.js
+  // append on day currently selected in task area
    
 
